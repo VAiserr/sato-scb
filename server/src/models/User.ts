c819253bf290d95beb675df4ba157dcb747380c5
Ivan <docpro.2002@gmail.com>
2022-12-14 14:23:03 +0300

    add user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Types } from "mongoose";
+
+interface IUser {
+  username: string;
+  accessKey: string;
+  password: string;
+  profs?: Types.ObjectId[];
+  avatar?: string;
+  isAdmin: boolean;
+  projects: Types.ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+  username: { type: String, required: false, unique: true },
+  accessKey: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profs: [{type: Schema.Types.ObjectId, ref: "Prof"}],
+  avatar: {type: String},
+  isAdmin: {type: Boolean, default: false},
+  projects: [{type: Schema.Types.ObjectId, ref: "Project"}]
+},
+{timestamps: true}
+);
+
+const User = model<IUser>("User", userSchema);
+
+export {User, IUser};
